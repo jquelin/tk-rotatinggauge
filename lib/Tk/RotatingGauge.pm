30d61798ba36605718ff81c2cc2890d945158d72
Jérôme Quelin <jquelin@gmail.com>
2007-09-06 11:13:25 +0000

    r6115@merlin:  jquelin | 2007-09-06 12:36:09 +0200  initial coding - kinda slow, but works

--- lib/Tk/RotatingGauge.pm
+++ lib/Tk/RotatingGauge.pm
@@ -11,7 +11,67 @@ package Tk::RotatingGauge;
 use warnings;
 use strict;
 
+use POSIX qw[ floor ];
+use Tk;
+use Tk::Canvas;
+
+use base qw[ Tk::Derived Tk::Canvas ];
+Construct Tk::Widget 'RotatingGauge';
+
+
 our $VERSION   = '0.01';
+
+sub Populate {
+    my( $self, $args ) = @_;
+
+
+    $self->SUPER::Populate( $args );
+    $self->ConfigSpecs(
+        -from    => [ 'PASSIVE', undef, undef, 0   ],
+        -visible => [ 'PASSIVE', undef, undef, 20  ],
+        -to      => [ 'PASSIVE', undef, undef, 100 ],
+        -value   => [ 'METHOD',  undef, undef, undef ],
+    );
+    my $val = exists $args->{-value} ? delete $args->{-value} : 50 ;
+    $self->afterIdle( sub{ $self->configure(-value => $val) } );
+
+    my $w = $self->cget('-width');
+    my $h = $self->cget('-height');
+    $self->createLine( 0, 1,  $w, 1  );
+    $self->createLine( 0, $h, $w, $h );
+    $self->afterIdle( sub {
+        my $w = $self->cget('-width');
+        $self->createLine( $w/2, 0, $w/2, $h, -fill=>'red', -width=>2)
+    } );
+}
+
+
+sub value {
+    my ($self, $value) = @_;
+
+    my $from    = $self->cget('-from');
+    my $to      = $self->cget('-to');
+
+    #$value = $from if $value < $from;
+    #$value = $to   if $value > $to;
+
+    $self->delete( 'tick' );
+
+    my $w = $self->{Configure}{-width};
+    my $h = $self->{Configure}{-height};
+    my $visible = $self->{Configure}{-visible};
+
+    my $step = $w / $visible;
+    my $frac = $value - int($value);
+    my $num  = ( floor( $value - ($visible/2) ) + 1 ) % $to;
+
+    foreach my $i ( 0 .. $visible ) {
+        my $x   = ( 1 - $frac + $i) * $step;
+        $self->createLine( $x, 0, $x, $h, -tags=>'tick' );
+        $self->createText( $x+$step/2, $h/2, -text=>$num, -tags=>'tick' );
+        $num = ($num+1) % $to;
+    }
+}
 
 
 
