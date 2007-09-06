8bb698fd593fa8fdf66d5c37de2ddc9202454e0e
Jérôme Quelin <jquelin@gmail.com>
2007-09-06 11:13:16 +0000

    r6114@merlin:  jquelin | 2007-09-06 12:26:02 +0200  module stub

---
+++ lib/Tk/RotatingGauge.pm
@@ -0,0 +1,104 @@
+#
+# This file is part of Tk::RotatingGauge
+# Copyright (c) 2007 Jerome Quelin, all rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
+#
+
+package Tk::RotatingGauge;
+
+use warnings;
+use strict;
+
+our $VERSION   = '0.01';
+
+
+
+1;
+__END__
+
+=head1 NAME
+
+Tk::RotatingGauge - a rotating gauge for Tk
+
+
+
+=head1 SYNOPSIS
+
+    use Tk::RotatingGauge;
+
+    my $g = $mw->RotatingGauge(
+    );
+
+
+
+=head1 DESCRIPTION
+
+This perl module provides a new Tk widget representing a gauge where the
+current value always stays at the same place. Think about your old
+mileage counters...
+
+
+
+
+=head1 STANDARD OPTIONS
+
+=head1 WIDGET-SPECIFIC OPTIONS
+
+=head1 METHODS
+
+
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<< < bug-tk-rotatinggauge at
+rt.cpan.org> >>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Tk-RotatingGauge>.  I will be
+notified, and then you'll automatically be notified of progress on your
+bug as I make changes.
+
+
+
+=head1 SEE ALSO
+
+C<Tk::RotatingGauge> development takes place on
+L<http://tk-rotatinggauge.googlecode.com> - feel free to join us.
+
+
+You can also look for information on this module at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Tk-RotatingGauge>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Tk-RotatingGauge>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Tk-RotatingGauge>
+
+=back
+
+
+
+=head1 AUTHOR
+
+Jerome Quelin, C<< <jquelin at cpan.org> >>
+
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (c) 2007 Jerome Quelin, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+
+=cut
+
