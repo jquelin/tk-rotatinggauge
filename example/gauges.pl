0cb24816c91750806474e84411bf5de7ea3dd0b6
Jérôme Quelin <jquelin@gmail.com>
2007-09-07 12:51:46 +0000

    r6159@merlin:  jquelin | 2007-09-06 15:53:42 +0200  example provided

---
+++ example/gauges.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+#
+# This file is part of Tk::RotatingGauge
+# Copyright (c) 2007 Jerome Quelin, all rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
+#
+
+use strict;
+use warnings;
+
+use FindBin qw[ $Bin ];
+use lib "$Bin/../lib";
+
+use Tk;
+use Tk::RotatingGauge;
+
+my ($w, $h)     = (200, 30);
+my ($f, $t, $v) = (0, 10, 4);
+my $val = 5;
+
+my $mw = Tk::MainWindow->new;
+$mw->Button(-command=>\&minus, -text=> '-')->pack(-side=>'left');
+my $g = $mw->RotatingGauge(
+    -width   => $w,
+    -height  => $h,
+    -from    => $f,
+    -to      => $t,
+    -visible => $v,
+    -value   => $val,
+)->pack(-side=>'left', -expand=>1, -fill=>'both');
+$mw->Button(-command=>\&plus,  -text=> '+')->pack(-side=>'right');
+
+MainLoop;
+exit;
+
+sub minus { $val-=0.1; $g->value($val); }
+sub plus  { $val+=0.1; $g->value($val); }
+
