44504eca792fbc12e77f61f7da17c0c59f1804b7
Jérôme Quelin <jquelin@gmail.com>
2007-09-07 12:53:18 +0000

    r6166@merlin:  jquelin | 2007-09-07 11:44:17 +0200  added a checkbox to the example to show the policy flag

--- example/gauges.pl
+++ example/gauges.pl
@@ -30,7 +30,15 @@ my $g = $mw->RotatingGauge(
     -visible => $v,
     -value   => $val,
 )->pack(-side=>'left', -expand=>1, -fill=>'both');
-$mw->Button(-command=>\&plus,  -text=> '+')->pack(-side=>'right');
+$mw->Button(-command=>\&plus,  -text=> '+')->pack(-side=>'left');
+my $policy = 'rotate';
+$mw->Checkbutton(
+    -text     => 'strict',
+    -variable => \$policy,
+    -onvalue  => 'strict',
+    -offvalue => 'rotate',
+    -command  => sub { $g->configure(-policy=>$policy); },
+)->pack(-side=>'left');
 
 MainLoop;
 exit;
