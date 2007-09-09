dec9001fa462e75a94a7089890ec40d7accbd3ab
Jérôme Quelin <jquelin@gmail.com>
2007-09-09 09:04:49 +0000

    r6190@merlin:  jquelin | 2007-09-07 18:03:34 +0200  added an example labeled gauge

--- example/gauges.pl
+++ example/gauges.pl
@@ -19,30 +19,52 @@ use Tk::RotatingGauge;
 my ($w, $h)     = (200, 30);
 my ($f, $t, $v) = (2, 7, 3);
 my $val = 5;
-
 my $mw = Tk::MainWindow->new;
-$mw->Button(-command=>\&minus, -text=> '-')->pack(-side=>'left');
-my $g = $mw->RotatingGauge(
-    -width   => $w,
-    -height  => $h,
-    -from    => $f,
-    -to      => $t,
-    -visible => $v,
-    -value   => $val,
+my $fr;
+my ($i, @g, @val, @pol);
+
+#
+$i = 0;
+$fr = $mw->Frame->pack;
+$g[$i] = $fr->RotatingGauge(
+    -width   => $w, -height  => $h,
+    -from    => $f, -to      => $t,
+    -visible => $v, -value   => $val[$i]=$val,
 )->pack(-side=>'left', -expand=>1, -fill=>'both');
-$mw->Button(-command=>\&plus,  -text=> '+')->pack(-side=>'left');
-my $policy = 'rotate';
-$mw->Checkbutton(
+$fr->Button(-command=>sub{minus(0)}, -text=> '-')->pack(-side=>'left');
+$fr->Button(-command=>sub{plus(0)},  -text=> '+')->pack(-side=>'left');
+$pol[$i] = 'rotate';
+$fr->Checkbutton(
     -text     => 'strict',
-    -variable => \$policy,
+    -variable => \$pol[$i],
     -onvalue  => 'strict',
     -offvalue => 'rotate',
-    -command  => sub { $g->configure(-policy=>$policy); },
+    -command  => sub { $g[$i]->configure(-policy=>$pol[$i]); },
+)->pack(-side=>'left');
+
+#
+$i = 1;
+$fr = $mw->Frame->pack;
+$g[$i] = $fr->RotatingGauge(
+    -width   => $w, -height  => $h,
+    -from    => $f, -to      => $t,
+    -visible => $v, -value   => $val[$i]=$val,
+    -labels => [ qw[ foo bar two three four five six ] ],
+)->pack(-side=>'left', -expand=>1, -fill=>'both');
+$fr->Button(-command=>sub{minus(1)}, -text=> '-')->pack(-side=>'left');
+$fr->Button(-command=>sub{plus(1)},  -text=> '+')->pack(-side=>'left');
+$pol[$i] = 'rotate';
+$fr->Checkbutton(
+    -text     => 'strict',
+    -variable => \$pol[$i],
+    -onvalue  => 'strict',
+    -offvalue => 'rotate',
+    -command  => sub { $g[$i]->configure(-policy=>$pol[$i]); },
 )->pack(-side=>'left');
 
 MainLoop;
 exit;
 
-sub minus { $val-=0.1; $g->value($val); }
-sub plus  { $val+=0.1; $g->value($val); }
+sub minus { my ($i)=@_; $val[$i]-=0.1; $g[$i]->value($val[$i]); }
+sub plus  { my ($i)=@_; $val[$i]+=0.1; $g[$i]->value($val[$i]); }
 
