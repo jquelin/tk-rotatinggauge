e7bed1779f2926f89f98f00313ce63914e9b5b7c
Jérôme Quelin <jquelin@gmail.com>
2010-01-12 19:34:42 +0100

    make example code suck less

--- examples/gauges.pl
+++ examples/gauges.pl
@@ -11,66 +11,88 @@ use Tk::RotatingGauge;
 
 my ($w, $h)     = (200, 30);
 my ($f, $t, $v) = (2, 7, 3);
-my $val = 5;
 my $mw = Tk::MainWindow->new;
-my $fr;
-my ($i, @g, @val, @pol);
 
-#
-$i = 0;
-$fr = $mw->Frame->pack;
-$g[$i] = $fr->RotatingGauge(
-    -width   => $w, -height  => $h,
-    -from    => $f, -to      => $t,
-    -visible => $v, -value   => $val[$i]=$val,
-    -indicator => 'green', -box => 'yellow',
-)->pack(-side=>'left', -expand=>1, -fill=>'both');
-$fr->Button(-command=>sub{minus(0)}, -text=> '-')->pack(-side=>'left');
-$fr->Button(-command=>sub{plus(0)},  -text=> '+')->pack(-side=>'left');
-$pol[$i] = 'rotate';
-$fr->Checkbutton(
-    -text     => 'strict',
-    -variable => \$pol[$i],
-    -onvalue  => 'strict',
-    -offvalue => 'rotate',
-    -command  => sub { $g[$i]->configure(-policy=>$pol[$i]); },
-)->pack(-side=>'left');
+# prettyfying tk app.
+# see http://www.perltk.org/index.php?option=com_content&task=view&id=43&Itemid=37
+$mw->optionAdd('*BorderWidth' => 1);
 
-#
-$i = 1;
-$fr = $mw->Frame->pack;
-$g[$i] = $fr->RotatingGauge(
-    -width   => $w, -height  => $h,
-    -from    => $f, -to      => $t,
-    -visible => $v, -value   => $val[$i]=$val,
-    -labels => [ qw[ foo bar two three four five six ] ],
-)->pack(-side=>'left', -expand=>1, -fill=>'both');
-$fr->Button(-command=>sub{minus(1)}, -text=> '-')->pack(-side=>'left');
-$fr->Button(-command=>sub{plus(1)},  -text=> '+')->pack(-side=>'left');
-$pol[$i] = 'rotate';
-$fr->Checkbutton(
-    -text     => 'strict',
-    -variable => \$pol[$i],
-    -onvalue  => 'strict',
-    -offvalue => 'rotate',
-    -command  => sub { $g[$i]->configure(-policy=>$pol[$i]); },
-)->pack(-side=>'left');
 
-#
-$i = 2;
-$fr = $mw->Frame->pack;
-$g[$i] = $fr->RotatingGauge(
-    -width   => $h, -height  => $w,
-    -from    => $f, -to      => $t,
-    -visible => $v, -value   => $val[$i]=$val,
-    -orient  => 'vert',
-)->pack(-side=>'left', -expand=>1, -fill=>'both');
-$fr->Button(-command=>sub{minus(2)}, -text=> '-')->pack(-side=>'left');
-$fr->Button(-command=>sub{plus(2)},  -text=> '+')->pack(-side=>'left');
+# easy one
+{
+    my $frame  = $mw->Frame->pack;
+    my $policy = 'rotate';
+    my $val    = 5;
+    my $g = $frame->RotatingGauge(
+        -width   => $w, -height  => $h,
+        -from    => $f, -to      => $t,
+        -visible => $v, -value   => $val,
+        -indicator => 'green', -box => 'yellow',
+    )->pack(-side=>'left', -expand=>1, -fill=>'both');
+
+    $frame->Button(-text=>'-', -command=>sub{$val-=0.1;$g->value($val)})->pack(-side=>'left');
+    $frame->Button(-text=>'+', -command=>sub{$val+=0.1;$g->value($val)})->pack(-side=>'left');
+    $frame->Checkbutton(
+        -text     => 'strict',
+        -variable => \$policy,
+        -onvalue  => 'strict',
+        -offvalue => 'rotate',
+        -command  => sub { $g->configure(-policy=>$policy); },
+    )->pack(-side=>'left');
+}
+
+# with labels
+{
+    my $frame  = $mw->Frame->pack;
+    my $policy = 'rotate';
+    my $val    = 5;
+    my $g = $frame->RotatingGauge(
+        -width   => $w, -height  => $h,
+        -from    => $f, -to      => $t,
+        -visible => $v, -value   => $val,
+        -labels => [ qw[ foo bar two three four five six ] ],
+    )->pack(-side=>'left', -expand=>1, -fill=>'both');
+
+    $frame->Button(-text=>'-', -command=>sub{$val-=0.1;$g->value($val)})->pack(-side=>'left');
+    $frame->Button(-text=>'+', -command=>sub{$val+=0.1;$g->value($val)})->pack(-side=>'left');
+    $frame->Checkbutton(
+        -text     => 'strict',
+        -variable => \$policy,
+        -onvalue  => 'strict',
+        -offvalue => 'rotate',
+        -command  => sub { $g->configure(-policy=>$policy); },
+    )->pack(-side=>'left');
+}
+
+# some kind of mileage counter
+{
+    my $frame  = $mw->Frame->pack;
+    my $val = 0;
+    my $g1 = $frame->RotatingGauge(
+        -width   => $h, -height => 2 * $h,
+        -from    => 0,  -to     => 10,
+        -visible => $v, -value  => $val,
+        -orient  => 'vert',
+    )->pack(-side=>'left', -expand=>1, -fill=>'both');
+    my $g2 = $frame->RotatingGauge(
+        -width   => $h, -height  => 2 * $h,
+        -from    => 0,  -to      => 10,
+        -visible => $v, -value   => $val,
+        -orient  => 'vert',
+    )->pack(-side=>'left', -expand=>1, -fill=>'both');
+    my $g3 = $frame->RotatingGauge(
+        -width   => $h, -height  => 2 * $h,
+        -from    => 0,  -to      => 10,
+        -visible => $v, -value   => $val,
+        -orient  => 'vert',
+    )->pack(-side=>'left', -expand=>1, -fill=>'both');
+    $frame->repeat( 25, sub {
+        $val++;
+        $g1->value($val/1000);
+        $g2->value($val/100);
+        $g3->value($val/10);
+    } );
+}
 
 MainLoop;
 exit;
-
-sub minus { my ($i)=@_; $val[$i]-=0.1; $g[$i]->value($val[$i]); }
-sub plus  { my ($i)=@_; $val[$i]+=0.1; $g[$i]->value($val[$i]); }
-
