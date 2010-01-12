eb0ebd9f77f6d4bbda84b2d0b12a3ea88d5fb230
Jérôme Quelin <jquelin@gmail.com>
2010-01-12 17:40:10 +0100

    legal blurb now auto-added

--- lib/Tk/RotatingGauge.pm
+++ lib/Tk/RotatingGauge.pm
@@ -1,11 +1,3 @@
-#
-# This file is part of Tk::RotatingGauge
-# Copyright (c) 2007 Jerome Quelin, all rights reserved.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the same terms as Perl itself.
-#
-
 package Tk::RotatingGauge;
 
 use warnings;
